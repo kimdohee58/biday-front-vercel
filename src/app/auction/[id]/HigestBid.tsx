bd322425dc4a2884335ce28b2ef50d0e3ae1364e
pokitong <yujusul@gmail.com>
2024-10-04 09:37:17 +0900

    체크아웃용

--- src/app/auction/[id]/HigestBid.tsx
+++ src/app/auction/[id]/HigestBid.tsx
@@ -4,43 +4,44 @@ import {useEffect, useState} from "react";
 import {BidStreamModel} from "@/model/BidModel";
 
 
-export default function HighestBid(auctionId: string) {
+export default function HighestBid(auctionId:string) {
     const [highestBid, setHighestBid] = useState<number>();
     const [error, setError] = useState<string | null>(null);
 
-    const url = `${[process.env.NEXT_PUBLIC_API_SERVER_URL]}/api/bids/stream?auctionId=${Number(auctionId)}`
+    const url = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/bids/stream?auctionId=${Number(auctionId)}`
 
     useEffect(() => {
+        console.log("auctionId 확인", auctionId);
         console.log("useEffect 진입");
+
         const eventSource = new EventSource(url);
 
-        eventSource.onmessage = (event: MessageEvent) => {
-            console.log("메세지 탐");
+        eventSource.addEventListener("message", (event: MessageEvent) => {
+            console.log("데이터 수신 완료, 수신데이터: ", event.data);
             try {
-                const bidStreamModel: BidStreamModel = JSON.parse(event.data);
-                console.log("bidStreamModel",bidStreamModel)
-                const currentBid = bidStreamModel.currentBid;
-                if (!!currentBid) {
-                    setHighestBid(currentBid);
-                }
-
-                console.log("highestBid", highestBid);
+                const bidStream: BidStreamModel = JSON.parse(event.data);
+                const currentBid = bidStream.currentBid;
+                setHighestBid(currentBid);
 
             } catch (error) {
                 console.error("SSE 데이터 파싱 중 오류 발생", error);
                 setError("SSE 데이터 파싱 중 오류 발생");
-
             }
+        });
 
-            eventSource.onerror = () => {
-                console.error("SSE 에러", error);
-                setError("SSE 에러");
-            };
+        eventSource.addEventListener("open", () => {
+            console.log("SSE 연결 완료");
+        });
 
-        };
+        eventSource.addEventListener("error", () => {
+            console.error("SSE 오류 발생", error);
+            if (eventSource.readyState === EventSource.CLOSED) {
+                console.log("연결 종료");
+            }
+        });
 
         return () => {
-            console.log("SSE 연결 종료");
+            console.log("연결 종료")
             eventSource.close();
         };
 
@@ -48,11 +49,13 @@ export default function HighestBid(auctionId: string) {
 
     useEffect(() => {
         if (highestBid !== undefined) {
-            console.log("highestBid updated", highestBid);
+            console.log("최고입찰가 갱신", highestBid);
         }
     }, [highestBid]);
 
     return (
-        <div>{highestBid}</div>
+        <div>
+            {highestBid}
+        </div>
     );
 };
