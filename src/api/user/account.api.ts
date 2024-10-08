fe239c4313bc193a61f27092d451b226b975ca8e
pokitong <yujusul@gmail.com>
2024-10-08 20:06:58 +0900

    feat: 계좌 조회 및 계좌 등록 성공, 전략패턴에 따른 api 코드 부분 수정

--- src/api/user/account.api.ts
+++ src/api/user/account.api.ts
@@ -2,20 +2,42 @@
 import { api } from "../request";
 import { strategy } from "../api.strategy";
 import {AccountModel} from "@/model/user/account.model";
+import Cookies from "js-cookie";
 
 // 계좌 조회 (GET 요청)
-const UserId = async (userId: string): Promise<AccountModel> => {
-    const response = await strategy.GET(`${api.account}/${userId}`);
-    return response;
+const findById = async (userId: string, token:string): Promise<AccountModel> => {
+
+    console.log("토큰 확인: ", token);
+
+    const userInfo = {
+        userId: "6700e19686d1ce6cd1fc6f25",
+        userName: "shull",
+        userRole: "ROLE_USER",
+    }
+
+    const response = await strategy.GET(`${api.account}`, undefined, {'UserInfo' : JSON.stringify(userInfo)}, token);
+
+    const data = await response;
+
+    console.log("api/account/account.api.ts response 응답: ", data);
+
+    console.log("응답확인: ", data);
+
+    return data;
 };
 
 // 계좌 등록 (POST 요청)
-const save= async (accountData: AccountModel): Promise<AccountModel> => {
-    const response = await strategy.POST(`${api.account}/save`, accountData);
-    return response;
+const save = async (accountData: AccountModel): Promise<AccountModel> => {
+    const token = Cookies.get("token");
+    console.log("토큰 확인: ", token)
+    const response = await strategy.POST(`${api.account}/save`, accountData, undefined, undefined, token);
+
+    const data = await response.json();
+    console.log("응답확인: ",data);
+    return data;
 };
 
 export const accountAPI = {
-    UserId,
+    findById,
     save,
 };
