14c2bd5689f340b93992f50f9564406e2c1ec466
kimdohee58 <kimdohee58@gmail.com>
2024-10-16 17:46:57 +0900

    price : 원화표시 및 , ì§찍ã기/productCard : wishes 수 출력

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -96,7 +96,6 @@ const ProductCard: FC<ProductCardProps> = ({
             ),
             {
                 position: "top-right",
-                // id: String(id) || "product-detail",
                 id: String(id) || `/product/${id}`,
                 duration: 3000,
             }
@@ -293,12 +292,10 @@ const ProductCard: FC<ProductCardProps> = ({
             <div
                 className={`nc-ProductCard relative flex flex-col bg-transparent ${className}`}
             >
-                {/*<Link href={"/product-detail"} className="absolute inset-0"></Link>*/}
                 <Link href={`/product/${id}`} className="absolute inset-0"></Link>
 
                 <div
                     className="relative flex-shrink-0 bg-slate-50 dark:bg-slate-300 rounded-3xl overflow-hidden z-1 group">
-                    {/*<Link href={"/product-detail"} className="block">*/}
                     <Link href={`/product/${id}`} className="block">
                         <NcImage
                             containerClassName="flex aspect-w-11 aspect-h-12 w-full h-0"
@@ -330,8 +327,7 @@ const ProductCard: FC<ProductCardProps> = ({
                         <div className="flex items-center mb-0.5">
                             <StarIcon className="w-5 h-5 pb-[1px] text-amber-400"/>
                             <span className="text-sm ms-1 text-slate-500 dark:text-slate-400">
-                                {/*{rating || "dd"} ({numberOfReviews || 0} reviews)*/}
-                                {wishes || "dd"} ({numberOfReviews || 0} reviews)
+                                {wishes || "0"} ({numberOfReviews || 0} reviews)
                             </span>
                         </div>
                     </div>
