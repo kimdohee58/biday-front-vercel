0af0aa88b26fcf70e0e8c2e2ac1726ce3f131b5b
pokitong <yujusul@gmail.com>
2024-10-18 18:10:05 +0900

    chore: checkout 변경 확인용 머지

--- src/app/checkout/payment/Checkout.tsx
+++ src/app/checkout/payment/Checkout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {loadTossPayments, ANONYMOUS, TossPaymentsWidgets} from "@tosspayments/tosspayments-sdk";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 const clientKey = `${process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY}`;
 
@@ -10,7 +10,12 @@ interface Amount {
     value: number;
 }
 
-export default function Checkout({value, product, orderId, customerKey}: {value: number, product: string, orderId: string, customerKey: string}) {
+export default function Checkout({value, product, orderId, customerKey}: {
+    value: number,
+    product: string,
+    orderId: string,
+    customerKey: string
+}) {
     const [ready, setReady] = useState<boolean>(false);
     const [widgets, setWidgets] = useState<TossPaymentsWidgets | null>(null);
     const amount: Amount = {
@@ -18,10 +23,14 @@ export default function Checkout({value, product, orderId, customerKey}: {value:
         value: value,
     };
 
+    console.log("1");
+
     useEffect(() => {
         async function fetchPaymentWidgets() {
+            console.log("2")
             // ------  결제위젯 초기화 ------
             const tossPayments = await loadTossPayments(clientKey);
+            console.log("toss", tossPayments);
             // 회원 결제
             const widgets = tossPayments.widgets({
                 customerKey,
@@ -73,34 +82,40 @@ export default function Checkout({value, product, orderId, customerKey}: {value:
     return (
         <div className="wrapper w-1/3 mx-auto">
             <div className="box_section">
-                {/* 결제 UI */}
-                <div id="payment-method" />
-                {/* 이용약관 UI */}
-                <div id="agreement" />
+                {!ready ? (
+                        <div>Loading payment widget...</div>)
+                    :
+                    ( <>
+                        {/* 결제 UI */}
+                        <div id="payment-method"/>
+                        {/* 이용약관 UI */}
+                        <div id="agreement"/>
 
-                {/* 결제하기 버튼 */}
-                <button
-                    className="button"
-                    disabled={!ready}
-                    onClick={async () => {
-                        try {
-                            // ------ '결제하기' 버튼 누르면 결제창 띄우기 ------
-                            // 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
-                            // 결제 과정에서 악의적으로 결제 금액이 바뀌는 것을 확인하는 용도입니다.
-                            await widgets!.requestPayment({
-                                orderId: orderId,
-                                orderName: product,
-                                successUrl: window.location.origin + "/success",
-                                failUrl: window.location.origin + "/fail",
-                            });
-                        } catch (error) {
-                            // 에러 처리하기
-                            console.error(error);
-                        }
-                    }}
-                >
-                    야이새끼야
-                </button>
+                        {/* 결제하기 버튼 */}
+                        <button
+                            className="button"
+                            disabled={!ready}
+                            onClick={async () => {
+                                try {
+                                    // ------ '결제하기' 버튼 누르면 결제창 띄우기 ------
+                                    // 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
+                                    // 결제 과정에서 악의적으로 결제 금액이 바뀌는 것을 확인하는 용도입니다.
+                                    await widgets!.requestPayment({
+                                        orderId: orderId,
+                                        orderName: product,
+                                        successUrl: window.location.origin + "/success",
+                                        failUrl: window.location.origin + "/fail",
+                                    });
+                                } catch (error) {
+                                    // 에러 처리하기
+                                    console.error(error);
+                                }
+                            }}
+                        >
+                            야이새끼야
+                        </button>
+                    </>
+                    )}
             </div>
         </div>
     );
