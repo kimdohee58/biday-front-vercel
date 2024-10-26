49b838f4616c538e5dd9da058c8a85631659a312
jh328 <wnsgks32810@naver.com>
2024-10-26 16:58:12 +0900

    fix : brand.slice.ts 사용 안해서 삭제 했습니다. 쓸모 없는 주석 삭제 했습니다.

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -4,13 +4,11 @@ import userSlice from '@/lib/features/user.slice';
 import productSlice from "@/lib/features/product.slice";
 import wishSlice from "@/lib/features/wish.slice";
 import addressSlice from "@/lib/features/address.slice";
-import brandSlice from "@/lib/features/brand.slice";
 import categorySlice from "@/lib/features/category.slice";
 import faqSlice from "@/lib/features/faq.slice";
-import loginHistorySlice from "@/lib/features/loginHistory.slice";
 import ratingSlice from "@/lib/features/rating.slice";
 import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // 기본적으로 localStorage 사용
+import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
 import productCardSlice from "@/lib/features/productCard.slice";
 
@@ -27,23 +25,19 @@ import productCardSlice from "@/lib/features/productCard.slice";
 // export type AppDispatchProduct = typeof storeProduct.dispatch;
 
 
-// redux-persist 설정
 const persistConfig = {
     key: 'root',
-    storage,      // localStorage 사용
+    storage,
     whitelist: ['user'],
 };
 
-// 루트 리듀서 정의 (combineReducers 사용)
 const rootReducer = combineReducers({
     user: userSlice,
     product: productSlice,
     wish: wishSlice,
     address: addressSlice,
-    brand: brandSlice,
     category: categorySlice,
     faq: faqSlice,
-    loginHistory: loginHistorySlice,
     rating: ratingSlice,
     products: productSlice,
     productCards: productCardSlice,
@@ -51,25 +45,21 @@ const rootReducer = combineReducers({
 });
 
 
-// persistReducer로 루트 리듀서를 감싸줌
 const persistedReducer
     = persistReducer(persistConfig, rootReducer);
 
-// 스토어 생성
 export const store = configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
-            serializableCheck: false,  // 직렬화 경고 무시
+            serializableCheck: false,
             ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
         }),
-    devTools: process.env.NODE_ENV !== 'production',  // DevTools 활성화 여부 설정 (production 환경에서는 비활성화)
+    devTools: process.env.NODE_ENV !== 'production',
 });
 
-// redux-persist에서 사용할 persistor 생성
 export const persistor = persistStore(store);
 
-// 타입 정의
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
