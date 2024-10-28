e9d9d38ec313fdf4732110205b178fa1be325004
pokitong <yujusul@gmail.com>
2024-10-29 01:11:19 +0900

    fix: HighestBid에 useRef를 사용해 리렌더링 되어도 초기화되지 않게 변경

--- src/app/auction/[id]/HighestBid.tsx
+++ src/app/auction/[id]/HighestBid.tsx
@@ -1,45 +1,53 @@
 "use client";
 
 import {BidStreamModel} from "@/model/auction/bid.model";
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 
 interface HighestBidProps {
     auctionId: string;
     onDataUpdate: (data: { highestBid: number; adjustBid: number }) => void
 }
 export default function HighestBid ({auctionId, onDataUpdate}: HighestBidProps)  {
-    const url = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/bids/stream?auctionId=${auctionId}`;
+    const eventSourceRef = useRef<EventSource | null>(null);
+
 
     useEffect(() => {
-        const eventSource = new EventSource(url);
+        if (!eventSourceRef.current) {
+            const url = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/bids/stream?auctionId=${auctionId}`;
+            eventSourceRef.current = new EventSource(url);
 
-        eventSource.addEventListener("message", (event: MessageEvent) => {
-            try {
-                const bidStream: BidStreamModel = JSON.parse(event.data);
-                const newHighestBid = bidStream.currentBid;
-                const newAdjustBid = newHighestBid + 4000;
+            eventSourceRef.current.addEventListener("message", (event: MessageEvent) => {
+                try {
+                    const bidStream: BidStreamModel = JSON.parse(event.data);
+                    const newHighestBid = bidStream.currentBid;
+                    const newAdjustBid = newHighestBid + 4000;
 
-                // 상위 컴포넌트에 업데이트된 데이터 전달
-                onDataUpdate({ highestBid: newHighestBid, adjustBid: newAdjustBid });
-            } catch (error) {
-                console.error("SSE 데이터 파싱 중 오류 발생", error);
+                    onDataUpdate({highestBid: newHighestBid, adjustBid: newAdjustBid});
+                } catch (error) {
+                    console.error("SSE 데이터 파싱 중 오류 발생", error);
+                }
+            });
+
+            eventSourceRef.current.addEventListener("open", () => {
+                console.log("SSE 연결 완료");
+            });
+
+            eventSourceRef.current.addEventListener("error", () => {
+                console.error("SSE 오류 발생");
+                if (eventSourceRef.current && eventSourceRef.current.readyState === EventSource.CLOSED) {
+                    console.log("SSE 연결 종료");
+                    eventSourceRef.current = null;
+                }
+            } )
+
+            return () => {
+                if (eventSourceRef.current) {
+                    eventSourceRef.current.close();
+                    eventSourceRef.current = null;
+                }
             }
-        });
+        }
 
-        eventSource.addEventListener("open", () => {
-            console.log("SSE 연결 완료");
-        });
-
-        eventSource.addEventListener("error", () => {
-            console.error("SSE 오류 발생");
-            if (eventSource.readyState === EventSource.CLOSED) {
-                console.log("연결 종료");
-            }
-        });
-
-        return () => {
-            eventSource.close();
-        };
     }, [auctionId, onDataUpdate]);
 
     return null;
