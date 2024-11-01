e29b4e2706ce6ab6e5704e113ce6c8d0decc1a6f
pokitong <yujusul@gmail.com>
2024-11-01 16:28:03 +0900

    feat: 라이브러리 오류 자동 수정 스크립트 추가

--- scripts/modifyLibrary.ts
+++ scripts/modifyLibrary.ts
@@ -1,28 +1,141 @@
-import path from "path";
-import * as fs from "node:fs";
+const path = require("path");
+const fs = require("node:fs");
 
 interface Modification {
     filePath: string;
-    searchValue: string | RegExp;
+    searchValues: (string | RegExp)[];
 }
 
 const modifications: Modification[] = [
     {
+        filePath: "../node_modules/@material-tailwind/react/components/Avatar/index.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Typography/index.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Input/index.d.ts",
+        searchValues: [
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g,
+            /"crossOrigin"\s*\|?/g
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Select/index.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/IconButton/index.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Dialog/index.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g,
+            /"handler"\s*\|?/g
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Dialog/DialogHeader.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g,
+        ],
+    },
+    {
         filePath: "../node_modules/@material-tailwind/react/components/Dialog/DialogBody.d.ts",
-        searchValue: "",
-    }
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g,
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Dialog/DialogFooter.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g,
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Button/index.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g,
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Card/index.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g,
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Card/CardHeader.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g,
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Card/CardBody.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g,
+        ],
+    },
+    {
+        filePath: "../node_modules/@material-tailwind/react/components/Card/CardFooter.d.ts",
+        searchValues: [
+            /"placeholder"\s*\|?/g,
+            /"onPointerEnterCapture"\s*\|?/g,
+            /"onPointerLeaveCapture"\s*\|?/g,
+        ],
+    },
+
+
 ];
 
 const modifyFiles = () => {
-    modifications.forEach(({filePath, searchValue}) => {
+    modifications.forEach(({filePath, searchValues}) => {
         const resolvedPath = path.resolve(__dirname, filePath);
         try {
             let content = fs.readFileSync(resolvedPath, "utf-8");
 
-            content = content.replace(searchValue, "");
+            searchValues.forEach((searchValue) => {
+                content = content.replace(searchValue, "");
+            })
 
             fs.writeFileSync(resolvedPath, content, "utf-8");
-            console.log(`${resolvedPath} 파일에서 구문이 성공적으로 삭제되었습니다.`);
+            console.log(`${resolvedPath} 파일 수정 완료.`);
         } catch (error) {
             console.error(`${resolvedPath} 파일 수정 중 오류 발생:`, error);
             process.exit(1);
