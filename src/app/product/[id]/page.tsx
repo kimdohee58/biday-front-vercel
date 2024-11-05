91864c1dc83b07af040f222bc5452f7cc408b798
POKITONG <yujusul@gmail.com>
2024-11-05 17:18:07 +0900

    fix: hydration suspense 추가

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,6 +1,7 @@
 import {fetchProductDetails} from "@/service/product/product.service";
 import ProductClientComponent from "@/app/product/[id]/ProductClientComponent";
 import {dehydrate, HydrationBoundary, QueryClient} from "@tanstack/react-query";
+import {Suspense} from "react";
 
 export default async function ProductPage({params}: { params: { id: string} }) {
     const queryClient = new QueryClient();
@@ -10,7 +11,9 @@ export default async function ProductPage({params}: { params: { id: string} }) {
     return (
         <div>
             <HydrationBoundary state={dehydratedState}>
-                <ProductClientComponent productId={params.id}/>
+                <Suspense>
+                    <ProductClientComponent productId={params.id}/>
+                </Suspense>
             </HydrationBoundary>
 
         </div>
