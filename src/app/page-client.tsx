40a9ce98662f7c8110ca160e328035f73e2e29d0
POKITONG <yujusul@gmail.com>
2024-11-06 20:08:03 +0900

    fix: 스켈레톤 isLoading에 맞춰 수정

--- src/app/page-client.tsx
+++ src/app/page-client.tsx
@@ -49,7 +49,7 @@ function RandomProductsByCategory({category, isLoading}: { category: string, isL
             </div>
             <hr className="mt-4 border-slate-200 dark:border-slate-700"/>
             <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-x-8 gap-y-10 mt-8 lg:mt-10">
-                {!isLoading ? (
+                {!isLoading && products && products.length > 0 ? (
                     products.map((product) => (
                         <ProductCard key={product.product.id} {...product} />
                     ))
